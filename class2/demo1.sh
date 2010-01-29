c66984c57eeda54922bb30f31ada3eecb826c04c
Pito Salas <rps@salas.com>
2010-01-29 17:53:38 -0500

    More content still!

--- class2/demo1.sh
+++ class2/demo1.sh
@@ -4,6 +4,7 @@ curl http://brandeis.edu
 
 curl http://www.brandeis.edu
 
-What does curl do? 
-Why does it act differently from in the browser?
-Who handles HTTP result-codes?
+- What does curl do?
+- Why do brandeis.edu and www.brandeis.edu act differently?
+- Why does it act differently from in the browser?
+- Who handles HTTP result-codes?
