9de8faa068aaa448d39187b5b5be95c95adfbafc
Pito Salas <rps@salas.com>
2010-01-29 16:06:10 -0500

    Going gangbusters adding content :)

---
+++ class2/demo1.sh
@@ -0,0 +1,9 @@
+Compare these two:
+
+curl http://brandeis.edu
+
+curl http://www.brandeis.edu
+
+What does curl do? 
+Why does it act differently from in the browser?
+Who handles HTTP result-codes?
