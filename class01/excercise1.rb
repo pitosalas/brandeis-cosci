c088ccec553d5c889cb97d2172af18dc53a62f95
Pito Salas <rps@salas.com>
2010-04-12 14:59:50 -0400

    Major organization to align folders with syllabus

--- class01/excercise1.rb
+++ class01/excercise1.rb
@@ -1,1 +1,14 @@
+# Homework for class 1, JBS Web and Mobile Applications, Summer 2010, Brandeis University
+#
+# Author: Pito Salas
+# Date: April 1, 2010
+#
+# Description: Trivial program to demonstrate that I got my enviuronment to work correctly.
+#
+# Running: 
+#       $ ruby excercise1.rb
+#
+# Expected result:
+#       $ hello brandeis
+
 puts "hello brandeis "
---
+++ class01/excercise2.rb
@@ -0,0 +1,34 @@
+# Homework for class 2, JBS Web and Mobile Applications, Summer 2010, Brandeis University
+#
+# Author: Pito Salas
+# Date: April 1, 2010
+#
+# Description: Fortune Cookie Generator class
+#
+# Running: 
+#       $ ruby excercise2.rb
+#
+# Expected result:
+#       "a fortune"
+#       "another fortune"
+#
+# class Fortune: generates fortune cookie fortunes
+#
+class Fortune
+  
+# List of fortunes. Add more if you want
+  FORTUNES = ["You will learn Ruby soon", "You will get an A for this course", "The Redsox will win the superbowl"]
+
+# Return a randomly selected fortune from the collection  
+  def next_fortune
+    FORTUNES[(rand * FORTUNES.length).to_int]
+  end
+    
+end
+
+if __FILE__ == $0 
+  f = Fortune.new
+  puts f.next_fortune
+  puts f.next_fortune
+end
+    
