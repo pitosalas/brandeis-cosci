47fe694ef6298fa5d896c3c44e4815f1598fbc72
Pito Salas <rps@salas.com>
2010-04-28 16:17:25 -0400

    [MORE] Continued

---
+++ class01/simpleserver.rb
@@ -0,0 +1,18 @@
+# Simple Server - Pito Salas
+#
+# This is a very trivial server that listens on port 888 and simply responds with the time of day at the server.
+# 
+# To Run:
+# ruby simplerver.rb
+#
+# To test, just go into your browser and go to url: 0.0.0.0:8888
+#
+require 'socket'               # Get sockets from stdlib
+
+server = TCPServer.open(8888)  # Socket to listen on port 2000
+loop {                         # Servers run forever
+  client = server.accept       # Wait for a client to connect
+  client.puts(Time.now.ctime)  # Send the time to the client
+  client.puts "Closing the connection. Bye!"
+  client.close                 # Disconnect from the client
+}
